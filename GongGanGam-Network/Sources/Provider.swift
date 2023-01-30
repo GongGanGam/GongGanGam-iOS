7588800b5c3d6fcf673b99a80a890d154a4d651b
chansooo <kcs05008@gmail.com>
2023-01-30 19:02:34 +0900

    fix: request 함수 제네릭하게 type 파라미터로 받도록 변경

--- GongGanGam-Network/Sources/Provider.swift
+++ GongGanGam-Network/Sources/Provider.swift
@@ -11,30 +11,30 @@ import Foundation
 import RxCocoa
 import RxSwift
 
-public protocol Provider: AnyObject {
-    
-    func request<T: Decodable>(endpoint: Endpoint, intercepter: RequestInterceptor?) -> Single<T>
-    func request<T: Decodable>(_ urlRequest: URLRequest, intercepter: RequestInterceptor?) -> Single<T>
-}
+//public protocol Provider: AnyObject {
+//
+//    func request<T: Decodable>(endpoint: Endpoint, intercepter: RequestInterceptor?) -> Single<T>
+//    func request<T: Decodable>(_ urlRequest: URLRequest, type: T.Type, intercepter: RequestInterceptor?) -> Single<T>
+//}
 
 
-final class ProviderImpl: Provider {
+public class NetworkManager {
     
     // MARK: Properties
     private let session: URLSession
     
-    static let `default` = ProviderImpl()
+//    static let `default` = ProviderImpl()
 
     // MARK: Initializers
-    init() {
+    public init() {
         let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest = 10
-        configuration.timeoutIntervalForResource = 10
+        configuration.timeoutIntervalForRequest = 100
+        configuration.timeoutIntervalForResource = 100
         self.session = URLSession(configuration: configuration)
     }
     
     // MARK: Methods
-    func request<T: Decodable>(endpoint: Endpoint, intercepter: RequestInterceptor? = nil) -> Single<T> {
+    public func request<T: Decodable>(endpoint: Endpoint, intercepter: RequestInterceptor? = nil) -> Single<T> {
         guard let req = try? endpoint.toURLRequest() else { return Single.error(NetworkError.invalidURL) }
         
         var requestUrl = req
@@ -43,13 +43,19 @@ final class ProviderImpl: Provider {
             requestUrl = intercepter.adapt(request: requestUrl)
         }
         
-        return self.request(requestUrl, intercepter: intercepter)
+        return self.request(requestUrl, type: T.self, intercepter: intercepter)
     }
     
-    func request<T: Decodable>(_ urlRequest: URLRequest, intercepter: RequestInterceptor? = nil) -> Single<T> {
+    public func request<T: Decodable>(_ urlRequest: URLRequest, type: T.Type, intercepter: RequestInterceptor? = nil) -> Single<T> {
         
         return self.session.rx.response(request: urlRequest)
-            .map { $0.data }
+            .map { res in
+                print(res)
+                if (200...299) ~= res.response.statusCode {
+                    return res.data
+                }
+                throw NetworkError.invalidStatusCode(res.response.statusCode)
+            }
             .decode(type: T.self, decoder: JSONDecoder())
             .asSingle()
             .catch({ err in
