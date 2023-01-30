802b37036cc6a72d9f607cd875fa0867381b968d
chansooo <kcs05008@gmail.com>
2023-01-30 19:05:43 +0900

    feat: baseURL 설정

---
+++ GongGanGam-Kit/Sources/Common/URL+.swift
@@ -0,0 +1,16 @@
+//
+//  URL+.swift
+//  GongGanGam
+//
+//  Created by kimchansoo on 2023/01/26.
+//  Copyright © 2023 GongGanGam. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    
+    static var baseUrl: URL? {
+        return URL(string: "http://18.189.150.89:8080/api/")
+    }
+}
