e88c4b47c5ee96643bccd0ab29c7c3b8e9f04035
chansooo <kcs05008@gmail.com>
2023-02-01 16:30:38 +0900

    chore: 테스트 해볼 수 있는 KitSample 구현

--- GongGanGam-Kit/Sources/Sample/KitSample.swift
+++ GongGanGam-Kit/Sources/Sample/KitSample.swift
@@ -16,7 +16,7 @@ import TokenManager
 public final class KitSample {
     
     // MARK: Properties
-    let diaryEndpoint: DiaryEndpoint
+    let diaryDataSource: DiaryRemoteDataSourceImpl
     let networkManager = NetworkManager()
     let tokenManager = KeychainTokenManager()
     let interceptor: GGGInterceptor
@@ -24,18 +24,61 @@ public final class KitSample {
     
     // MARK: Initializers
     public init() {
+        self.interceptor = GGGInterceptor(tokenManager: tokenManager, networkManager: networkManager)
+        self.diaryDataSource = DiaryRemoteDataSourceImpl(
+            networkManager: self.networkManager,
+            interceptor: self.interceptor
+        )
+        self.tokenUpdate()
+        
+//        self.writeDiary()
+        
+        self.fetchMonthly()
+            .subscribe { diary in
+                print("성공")
+                print("diary: \(diary)")
+            } onFailure: { err in
+                print("에러: \(err)")
+            } onDisposed: {
+                print("disposed")
+            }
+            .disposed(by: disposebag)
+        
+    }
+    // MARK: Methods
+    
+    private func tokenUpdate() {
         let access = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZGVudGlmaWNhdGlvbiI6IjIzNTQ2NDg4MjQiLCJyb2xlIjpbIlJPTEVfQURNSU4iXSwiaXNzIjoiZ29uZ2dhbmdhbSIsImlkIjoxLCJleHAiOjE3MDY1OTM4MDgsImlhdCI6MTY3NTA1NzgwOH0.jW_oJxRo02_rqRzkonUzGk6nWFpZNV0HFlnOOxPWAIc"
         let refresh = "1"
         tokenManager.save(token: access, with: .accessToken)
         tokenManager.save(token: refresh, with: .refreshToken)
-        self.interceptor = GGGInterceptor(tokenManager: tokenManager, networkManager: networkManager)
-        self.diaryEndpoint = DiaryEndpoint.fetchDiary(year: 2023, month: 01)
-        let stream: Single<DiaryResponseDTO>
-        stream = networkManager.request(endpoint: self.diaryEndpoint, intercepter: interceptor)
-        stream.subscribe { dto in
-            print(dto)
-        }
-        .disposed(by: disposebag)
     }
-    // MARK: Methods
+    
+    private func writeDiary() {
+        let diary = Diary(
+            date: Date(),
+            emoji: "즐거워요",
+            content: "나는 김형기이고, 지금 아주 즐겁습니다!",
+            shareAgreed: true
+        )
+        diaryDataSource.writeDiary(diary: diary, imageData: nil)
+            .subscribe { diary in
+                print("성공")
+                print("diary: \(diary)")
+            } onFailure: { err in
+                print("에러: \(err)")
+            } onDisposed: {
+                print("disposed")
+            }
+            .disposed(by: disposebag)
+
+    }
+    
+    private func fetchMonthly() -> Single<[Diary]> {
+        let endpoint = DiaryEndpoint.fetchDiary(year: 2023, month: 2)
+        return networkManager.request(endpoint: endpoint, intercepter: self.interceptor)
+            .map { (dtos: [DiaryResponseDTO]) -> [Diary] in
+                dtos.map { $0.toModel() }
+            }
+    }
 }
